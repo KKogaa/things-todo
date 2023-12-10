9d650e7c20992c7ccffa43cdf46fc070e3927e02
KKogaa <akoga@pucp.edu.pe>
2023-12-10 18:27:07 -0500

    feat: Added gin to backend

---
+++ things-todo-backend/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Start() {
+	instance := gin.New()
+	instance.Use(gin.Recovery())
+
+	srv := &http.Server{
+		Addr:    ":" + "8080",
+		Handler: instance,
+	}
+
+	log.Printf("server started on port: %s\n ", "8080")
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}
